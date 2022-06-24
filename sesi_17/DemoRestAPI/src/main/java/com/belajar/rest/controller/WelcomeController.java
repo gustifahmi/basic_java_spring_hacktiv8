1e35d80e0ef57da1430ec040bf98c00e847a507e
Gusti Fahmi Fadhila <gustifahmifadhila@gmail.com>
2022-06-24 15:23:16 +0700

    add landing page in sesi_17

---
+++ sesi_17/DemoRestAPI/src/main/java/com/belajar/rest/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.belajar.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/")
+	public String landing() {
+		return "redirect:/index";
+	}
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
